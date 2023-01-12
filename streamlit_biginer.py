8cd137fe021a625fc31e9003bac6bed028561059
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-01-12 09:34:25 +0900

    Update streamlit_biginer.py

--- streamlit_biginer.py
+++ streamlit_biginer.py
@@ -6,9 +6,9 @@ st.title('Streamlit 超入門')
 
 st.write('DataFrame')
 
-df = pd.DataFrame({
-    '1列目': [1, 2, 3, 4],
-    '2列目': [10, 20, 30, 40]
-})
+df = pd.DataFrame(
+    np.random(20, 3)
+    columns=('a', 'b','c')
+)
 
-st.table(df.style.highlight_max(axis=0))
+#st.table(df.style.highlight_max(axis=0))
