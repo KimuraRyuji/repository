eb9b565d9ecb9a45e20a8d56b1080d97ac51745c
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-01-13 08:48:21 +0900

    Update streamlit_biginer.py

--- streamlit_biginer.py
+++ streamlit_biginer.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from PIL import Image
 
 st.title('Streamlit TEST')
+
+st.write('Display Image')
+
+img = Image.open('C:\Users\rykimura\Downloads\AISmiley_BDSP_Point1.jpg')
+st.image(img, caption='rykimura', use_column_width=True)
 
 st.write('DataFrame')
 
