1a70cb69318c0d50e2397886a4f72618f13cedf8
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-01-13 23:22:53 +0900

    Update streamlit_biginer.py

--- streamlit_biginer.py
+++ streamlit_biginer.py
@@ -9,12 +9,12 @@ st.write('Display Image')
 
 st.write('Interactive Widgets')
 
-left_column, right_column = st.beta_columns(2)
+left_column, right_column = st.columns(2)
 button = left_column.button('右カラムに文字を表示')
 if button:
     right_column.write('ここは右カラム')
 
-expander = st.beta_expander('問い合わせ')
+expander = st.expander('問い合わせ')
 expander.write('問い合わせ内容を書く')
 
 text = st.text_input('あなたの趣味を教えてください')
