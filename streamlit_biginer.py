96eedc44c567d1e0abd9d50437bf6f95733c733a
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-01-13 23:20:42 +0900

    Update streamlit_biginer.py

--- streamlit_biginer.py
+++ streamlit_biginer.py
@@ -12,10 +12,10 @@ st.write('Interactive Widgets')
 left_column, right_column = st.beta_columns(2)
 button = left_column.button('右カラムに文字を表示')
 if button:
-    right_column.write('ここは右カラム')
+    right_column.st.write('ここは右カラム')
 
 expander = st.beta_expander('問い合わせ')
-expander.write('問い合わせ内容を書く')
+expander.st.write('問い合わせ内容を書く')
 
 text = st.text_input('あなたの趣味を教えてください')
 condition = st.slider('あなたの今の調子は？', 0, 100, 50)
