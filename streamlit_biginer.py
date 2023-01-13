6d3ff4726bccda4d562e13e7644a239393393ea6
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-01-13 18:24:01 +0900

    Update streamlit_biginer.py

--- streamlit_biginer.py
+++ streamlit_biginer.py
@@ -7,9 +7,14 @@ st.title('AI開発・運用')
 
 st.write('Display Image')
 
-if st.checkbox('Show Image'):
-    img = Image.open('AISmiley_BDSP_Point1.jpg')
-    st.image(img, caption='CRISP-DM', use_column_width=True)
+st.selectbox(
+    'あなたが好きな数字を教えてください',
+    list(range(1, 11))
+)
+'あなたの好きな数字は、', option, 'です。'
+#if st.checkbox('Show Image'):
+#    img = Image.open('AISmiley_BDSP_Point1.jpg')
+#    st.image(img, caption='CRISP-DM', use_column_width=True)
 
 st.write('DataFrame')
 
