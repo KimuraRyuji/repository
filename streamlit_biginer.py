80e50435e383936780c43621cc95514b9d0379e0
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-01-13 18:30:55 +0900

    Update streamlit_biginer.py

--- streamlit_biginer.py
+++ streamlit_biginer.py
@@ -12,6 +12,9 @@ st.write('Interactive Widgets')
 text = st.text_input('あなたの趣味を教えてください')
 'あなたの趣味：', text,'です。'
 
+condition = st.slider('あなたの今の調子は？', 0, 100, 50)
+'コンディション：', condition
+
 option = st.selectbox(
     'あなたが好きな数字を教えてください',
     list(range(1, 11))
