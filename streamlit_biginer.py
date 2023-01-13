102fafc0e56533a08911337712116902d718dda9
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-01-13 23:19:18 +0900

    Update streamlit_biginer.py

--- streamlit_biginer.py
+++ streamlit_biginer.py
@@ -14,6 +14,9 @@ button = left_column.button('右カラムに文字を表示')
 if button:
     right_column.write('ここは右カラム')
 
+expander = st.beta_expander('問い合わせ')
+expander.write('問い合わせ内容を書く')
+
 text = st.text_input('あなたの趣味を教えてください')
 condition = st.slider('あなたの今の調子は？', 0, 100, 50)
 
