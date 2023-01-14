a4bcc4b6ee286a789fb3ed220f4d3ccbb4f9a3a8
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-01-14 23:43:33 +0900

    Update streamlit_biginer.py

--- streamlit_biginer.py
+++ streamlit_biginer.py
@@ -2,12 +2,24 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from PIL import Image
+import time
 
 st.title('AI開発・運用')
 
 st.write('Display Image')
 
-st.write('Interactive Widgets')
+st.write('プログレスバーの表示')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteratin {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
+'done!!'
 
 left_column, right_column = st.columns(2)
 button = left_column.button('右カラムに文字を表示')
