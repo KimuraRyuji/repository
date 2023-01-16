2f455c67d089ae90dfc8fdb4f4cafab6e2979f6c
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-01-17 08:57:39 +0900

    Update streamlit_biginer.py

--- streamlit_biginer.py
+++ streamlit_biginer.py
@@ -1,11 +1,1 @@
 pip install streamlit
-
-import streamlit as st
-import numpy as np
-import pandas as pd
-
-df = pd.DataFrame(
-  np.random.rand(100, 2)/[50, 50] + [35.69, 139.70],
-  columuns=['lat', 'lon']
-)
-st.map(df)
