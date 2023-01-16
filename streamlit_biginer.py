1c8231761aa6a4cf3929492fad557a38b7e8896f
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-01-16 13:52:28 +0900

    Update streamlit_biginer.py

--- streamlit_biginer.py
+++ streamlit_biginer.py
@@ -1,13 +1,1 @@
-pip install streamlit
-
-import streamlit as st
-import numpy as np
-import pandas as pd
-
-st.write('テストアプリケーション')
-
-df = pd.DataFrame(
-  np.random.rand(100, 2)/[50, 50] + [35.69, 139.70],
-  columuns=['lat', 'lon']
-)
-st.map(df)
+quit()
