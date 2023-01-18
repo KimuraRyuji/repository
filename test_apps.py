88f78e7c2667c3a72476bba8a1a1f51bf5f24d9c
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-01-18 12:45:09 +0900

    Update test_apps.py

--- test_apps.py
+++ test_apps.py
@@ -2,6 +2,9 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+st.set_page_config(layout="wide")
+radius = st.sidebar.slider('現在地からの距離', 0, 20, 10)
+
 df = pd.DataFrame(
   np.random.randn(50, 2)/[50, 50] + [35.69, 139.70],
   columns=['lat', 'lon']
