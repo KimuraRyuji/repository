acdbf7e9ec1349208fda5aa1b197b13efd6ebce2
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-01-18 12:50:22 +0900

    Update test_apps.py

--- test_apps.py
+++ test_apps.py
@@ -5,6 +5,7 @@ import pandas as pd
 st.set_page_config(layout="wide")
 radius = st.sidebar.slider('現在地からの距離', 0, 20, 10)
 
+st.write('提携病院のパーソナライズドレコメンド')
 df = pd.DataFrame(
   np.random.randn(50, 2)/[50, 50] + [35.69, 139.70],
   columns=['lat', 'lon']
