8f071910780f99c082b9b1a9f8eff13333d284e8
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-01-19 08:38:20 +0900

    Update stock_visualization.py

--- stock_visualization.py
+++ stock_visualization.py
@@ -1,1 +1,39 @@
+pip install yfinance
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
 
+days = 20
+tickers = {
+    'apple': 'AAPL',
+    'Meta': 'META',
+    'MicroSoft': 'msft',
+    'google': 'GOOGL',
+    'Netflix': 'NFLX',
+    'Amazon': 'AMZN'
+}
+
+def get_data(days, tickers):
+    df = pd.DataFrame()
+    for company in tickers.keys():
+        tkr = yf.Ticker(tickers[company])
+        hist = tkr.history(period=f'{days}d')
+        hist.index = hist.index.strftime('%d %B %Y')
+        hist = hist[['Close']]
+        hist.columns = [company]
+        hist = hist.T
+        hist.index.name = 'NAME'
+        df = pd.concat([df, hist])
+    return df
+  
+  days = 20
+tickers = {
+    'apple': 'AAPL',
+    'Meta': 'META',
+    'MicroSoft': 'msft',
+    'google': 'GOOGL',
+    'Netflix': 'NFLX',
+    'Amazon': 'AMZN'
+}
+
+get_data(days, tickers)
