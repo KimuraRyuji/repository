b6c22014427462dd7507abed409312a9a592b29c
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-01-18 12:51:15 +0900

    Update test_apps.py

--- test_apps.py
+++ test_apps.py
@@ -5,6 +5,8 @@ import pandas as pd
 st.set_page_config(layout="wide")
 radius = st.sidebar.slider('現在地からの距離', 0, 20, 10)
 
+st.sidebar.write('日商 太郎様')
+
 st.write('提携病院のパーソナライズドレコメンド')
 df = pd.DataFrame(
   np.random.randn(50, 2)/[50, 50] + [35.69, 139.70],
