e5f5425fcbb67900da21eacec3d57e91186e955f
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-01-18 13:09:39 +0900

    Update test_apps.py

--- test_apps.py
+++ test_apps.py
@@ -6,9 +6,10 @@ st.set_page_config(layout="wide")
 radius = st.sidebar.slider('現在地からの距離', 0, 20, 10)
 
 st.sidebar.write('日商 太郎様')
-st.sidebar.write('ボストンテリア')
-st.sidebar.write('エレクトロニクス君')
-st.sidebar.write('5歳')
+st.sidebar.write('犬種：ボストンテリア')
+st.sidebar.write('名前：エレクトロニクス君')
+st.sidebar.write('年齢：5歳')
+st.sidebar.write('性別：♂')
 
 st.write('提携病院のパーソナライズドレコメンド')
 df = pd.DataFrame(
