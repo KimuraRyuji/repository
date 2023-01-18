0ca9cc0fc8596c5f809758b93320e3478adb0d43
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-01-18 09:29:30 +0900

    Update test_apps.py

--- test_apps.py
+++ test_apps.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 df = pd.DataFrame(
-  np.random.rand(1000, 2)/[50, 50] + [35.69, 139.70],
+  np.random.rand(50, 2)/[50, 50] + [35.69, 139.70],
   columns=['lat', 'lon']
 )
 st.map(df)
