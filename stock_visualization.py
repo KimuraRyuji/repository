dcc8b774caf5321cd37458c76cb270844778beaf
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-02-08 09:50:45 +0900

    Update stock_visualization.py

--- stock_visualization.py
+++ stock_visualization.py
@@ -40,4 +40,10 @@ tickers = {
 stock_data = pd.DataFrame(get_data(days, tickers))
 stock_data = stock_data.T.reset_index()
 stock_data = pd.melt(stock_data, id_vars=['Date']
-st.table(stock_data)
+chart = alt.Chart(stock_data).mark_line().encode(
+    x="date:T",
+    y="price",
+    color="symbol"
+)
+
+st.altair_chart(chart, use_container_width=True)
