0da0bc6be543a857a6eb50326aa3077d1e353b1a
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-07-07 15:36:57 +0900

    Update reins_datavisualization.py

--- reins_datavisualization.py
+++ reins_datavisualization.py
@@ -2,6 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv('https://github.com/KimuraRyuji/repository/blob/main/reins_used_202003.csv')
+repo_url = 'https://github.com/KimuraRyuji/repository'
+csv_paht = '/blob/main/reins_used_202003.csv'
 
-st.table(data)
+def load_data():
+  df = pd.read_csv(csv_path)
+  return df
+
+data = load_data()
+
+st.bar_chart(data)
