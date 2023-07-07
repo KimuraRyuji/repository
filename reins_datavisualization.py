6afc8c662b4731886fc24c59254eb839ba925e66
KimuraRyuji <122063212+KimuraRyuji@users.noreply.github.com>
2023-07-07 12:52:08 +0900

    Update reins_datavisualization.py

--- reins_datavisualization.py
+++ reins_datavisualization.py
@@ -4,6 +4,6 @@ import numpy as np
 
 data = pd.read_csv('https://github.com/KimuraRyuji/repository/blob/main/reins_used_202003.csv')
 
-df = pd.DataFrame(data, columns=["年月", "件数"])
+df = pd.DataFrame(data, columns=[0, 1])
 
 st.dataframe(df)
